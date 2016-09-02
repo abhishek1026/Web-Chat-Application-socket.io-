08e65fcb1ccad6f42064cb93b2f6f77c86f5026f
Abhishek Patel <abhishek26@ufl.edu>
2016-09-02 02:00:22 -0400

    Third Version Released

--- index.js
+++ index.js
@@ -10,12 +10,15 @@ app.get('/', function (req, res) {
 io.on('connection', function (socket) {
   console.log('random user landed!');
 
-  socket.on('new user', function(data,callback){
+  socket.on('new user', function (data, callback) {
     console.log('username registration attempt');
-    if(nicknames.indexOf(data) != -1 || data==''){
-      callback(false);
+    if (nicknames.indexOf(data) != -1 || data == '') {
+      callback("taken");
     }
-    else{
+    else if (nicknames.length >= 10) {
+      callback("full");
+    }
+    else {
       socket.nickname = data;
       nicknames.push(socket.nickname);
       updateNicknames();
@@ -23,44 +26,123 @@ io.on('connection', function (socket) {
     }
   });
 
-  function updateNicknames(){
+  function updateNicknames() {
     io.sockets.emit('usernames', nicknames);
   }
 
   socket.on('chat message', function (msg) {
-    io.emit('chat message', {msg: msg,nick: socket.nickname});
-    console.log('message delivered by ' +socket.nickname+': '+ msg);
+    io.emit('chat message', { msg: msg, nick: socket.nickname });
+    console.log('message delivered by ' + socket.nickname + ': ' + msg);
   });
 
   socket.on('disconnect', function () {
-    if(!socket.nickname){
+    if (!socket.nickname) {
       console.log('random user fled!');
       return;
     }
     console.log(socket.nickname + ' disconnected');
-    nicknames.splice(nicknames.indexOf(socket.nickname),1);
+    io.emit('loser', socket.nickname);
+    nicknames.splice(nicknames.indexOf(socket.nickname), 1);
     updateNicknames();
   });
 
-  socket.on('userentry', function(user){
+  socket.on('userentry', function (user) {
     console.log(user + ' successfully joined chat room!');
   });
 
-  socket.on('invalid', function(crap){
+  socket.on('invalid', function (crap) {
     console.log('login attempt failed:');
-    if(crap != ''){
+    if (crap != '') {
       console.log(crap + ' is already in the system!');
     }
     else
       console.log('random user tried to enter blank username!');
   });
 
-  socket.on('message attempt', function(msg){
+  socket.on('message attempt', function (msg) {
     console.log(socket.nickname + ' is attempting to send a message!');
   });
 
-  socket.on('message fail', function(msg){
-    console.log(socket.nickname+ ' failed to send message because content is blank!');
+  socket.on('message fail', function (msg) {
+    console.log(socket.nickname + ' failed to send message because content is blank!');
+    io.emit('yousuck', socket.nickname);
+  });
+
+  socket.on('getUserColor', function (nick, callback) {
+
+    if (nicknames.indexOf(nick) == 0) {
+      callback("blue");
+    }
+    if (nicknames.indexOf(nick) == 1) {
+      callback("brown");
+    }
+    if (nicknames.indexOf(nick) == 2) {
+      callback("green");
+    }
+    if (nicknames.indexOf(nick) == 3) {
+      callback("pink");
+    }
+    if (nicknames.indexOf(nick) == 4) {
+      callback("purple");
+    }
+    if (nicknames.indexOf(nick) == 5) {
+      callback("maroon");
+    }
+    if (nicknames.indexOf(nick) == 6) {
+      callback("navy");
+    }
+    if (nicknames.indexOf(nick) == 7) {
+      callback("silver");
+    }
+    if (nicknames.indexOf(nick) == 8) {
+      callback("teal");
+    }
+    if (nicknames.indexOf(nick) == 9) {
+      callback("olive");
+    }
+
+  });
+
+  socket.on('full', function (user) {
+    console.log(user + ' cannot enter full lobby!');
+  });
+
+  socket.on('grabtime', function (some, callback) {
+    var time = new Date();
+    var hour = time.getHours();
+    var min = time.getMinutes();
+    if (min >= 0 && min < 10) {
+      min = "0" + min;
+    }
+    if (hour > 12) {
+      if (hour < 21) {
+        hour = "0" + (hour - 12);
+      }
+      else {
+        hour = (hour - 12);
+      }
+    }
+    else if (hour == 0) {
+      hour = "12";
+    }
+    else if (hour > 0 && hour < 10) {
+      hour = "0" + hour;
+    }
+    var ftime = hour + ":" + min;
+    if(some == socket.nickname){
+      console.log('message sent on: ' + ftime + " by " +some);
+    }
+    
+    callback(ftime);
+  });
+
+  socket.on('checkself', function (data, callback) {
+    if (socket.nickname == data.nick) {
+      callback(true);
+    }
+    else {
+      callback(false);
+    }
   });
 
 });
